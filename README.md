58c3a5150ad860a4784711b207d7f55a76e44118
theunissenb <benoit.theunissen@cgi.com>
2015-06-11 16:32:13 +0200

    initial import

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # android-sdk-updater
 Provides a simply script able to update the android sdk
+
+
+	./update.sh /android/home/path
---
+++ update.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+	echo "Please specify the android home path" > &2
+	exit 1
+fi
+
+ANDROID_SDK=$1
+
+shift 1
+
+ARGS="-s --no-ui $#"
+
+FILTERS="platform,tool,platform-tool,extra,add-on,source"
+
+# Cache the update list
+echo "Caching the update list..."
+"$ANDROID_SDK" --clear-cache list sdk $ARGS
+echo "ok"
+
+# Look for the build-tools id
+BUILD_TOOLS_FILTER=$($ANDROID_SDK list sdk $ARGS | grep "Build-tools" | cut -d '-' -f 1)
+if [ -z "$BUILD_TOOLS_FILTER" ]
+then
+  echo "Build-tools already exists"
+else
+  echo "Build-tools have to be updated '$BUILD_TOOLS_FILTER' "
+  FILTERS="$BUILD_TOOLS_FILTER,$FILTERS "
+fi
+
+expect -c "
+set timeout -1   ;
+spawn $ANDROID_SDK update sdk $ARGS -t $FILTERS
+expect {
+    \"Do you accept the license\" { exp_send \"y\r\" ; exp_continue }
+    eof
+}
+"
