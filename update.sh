c29bd438750b7d4cecb81700f14363226478225d
Benoit Theunissen <NeoMcFly@book.local>
2015-06-13 12:59:04 +0200

    ignore preview version

--- update.sh
+++ update.sh
@@ -2,38 +2,44 @@
 
 if [ $# -eq 0 ]
 then
-	echo "Please specify the android home path" >&2
-	exit 1
+  echo "Please specify the android home path" >&2
+  exit 1
 fi
 
 ANDROID_SDK=$1/tools/android
 
 shift 1
 
-ARGS="--no-ui $@"
+ARGS="$@"
 
 FILTERS="platform,tool,platform-tool,extra,add-on,source"
 
-# Cache the update list
-echo "Caching the update list..."
-"$ANDROID_SDK" --clear-cache list sdk $ARGS
-echo "ok"
+function update(){
 
-# Look for the build-tools id
-BUILD_TOOLS_FILTER=$($ANDROID_SDK list sdk $ARGS | grep "Build-tools" | head -n 1 | cut -d '-' -f 1)
+  echo "$ANDROID_SDK update sdk --no-ui $@"
+
+  expect -c "
+  set timeout -1   ;
+  spawn $ANDROID_SDK update sdk --no-ui $@
+  expect {
+      \"Do you accept the license\" { exp_send \"y\r\" ; exp_continue }
+     eof
+  }
+  "
+}
+
+# Check for Build-tools availability 
+BUILD_TOOLS_FILTER=$($ANDROID_SDK --clear-cache list sdk | grep -i build-tools | head -n 1 | cut -d '-' -f 1)
 if [ -z "$BUILD_TOOLS_FILTER" ]
 then
   echo "Build-tools is already installed and up to date"
 else
+  # Look for the build-tools id
+  BUILD_TOOLS_FILTER=$($ANDROID_SDK list sdk --all | grep -i build-tools | grep -v rc | head -n 1 | cut -d '-' -f 1)  
   echo "Build-tools have to be updated '$BUILD_TOOLS_FILTER' "
-  FILTERS="$BUILD_TOOLS_FILTER,$FILTERS "
+  update "--all -t $BUILD_TOOLS_FILTER"
 fi
 
-expect -c "
-set timeout -1   ;
-spawn $ANDROID_SDK update sdk $ARGS -t $FILTERS
-expect {
-    \"Do you accept the license\" { exp_send \"y\r\" ; exp_continue }
-    eof
-}
-"
+# Update the rest
+update $ARGS -t $FILTERS
+
